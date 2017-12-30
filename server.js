378440e320019553c315548995248c589ed05b08
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2017-12-29 19:50:39 -0500

    Set up mongodb infrastructure

--- server.js
+++ server.js
@@ -2,10 +2,22 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const port = 8000;
+const db = require('./config/db');
 
 const app = express();
+// Using body parser to parse HTTP requests since express cannot handle
+// HTTP requests.
+app.use(bodyParser.urlencoded({extended:true}));
 
-require('./app/routes')(app, {});
-app.listen(port, ()=>{
-    console.log('Live on port ' + port);
+
+MongoClient.connect(db.url, (err, database)=>{
+    if (err) return console.log(err);
+    // Specifying routes.
+    require('./app/routes')(app, {});
+
+    // Listening on port 8000
+    app.listen(port, ()=>{
+        console.log('Live on port ' + port);
+    });
+
 });
