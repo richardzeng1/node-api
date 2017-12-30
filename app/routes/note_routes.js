4392e5c9883323ecf298fe8393a8b99213db9b8b
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2017-12-29 22:28:14 -0500

    Implemented Create, Read, Update and Delete routes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,5 +1,53 @@
 modules.exports = function(app, db){
-    app.post('/notes', (req, req)=>{
-        res.send('Hello');
+    // Read
+    app.get('/notes/:id', (req, res)=>{
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.collection('notes').findOne(details, (err, item)=>{
+            if (err){
+                res.send({'error':'An error has occured'});
+            }else{
+                res.send(item)
+            }
+        });
+    });
+
+    // Create
+    app.post('/notes', (req, res)=>{
+        const note = {text: req.body.body, title: req.body.title};
+        db.collection('notes').insert(note, (err, result)=>{
+            if (err){
+                res.send({'error': 'An error has occured'});
+            }else{
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+    // Delete
+    app.delete('./notes/:id', (req, res)=>{
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.collection('notes').remove(details,(err,item)=>{
+            if (err){
+                res.send({'error':'An error has occured'});
+            }else{
+                res.send('Note ' + id + ' has been deleted');
+            }
+        });
+    });
+
+    // Update
+    app.put('./note/:id', (req, res)=>{
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        const note = {text:req.body.body, title:req.body.title};
+        db.collection('notes').update(details, note, (err, result)=>{
+            if (err){
+                res.send({'error':'An error has occured'});
+            }else{
+                res.send(note);
+            }
+        });
     });
 };
